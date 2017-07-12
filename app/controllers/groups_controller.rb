5fb1ff73e51776ccb85a7e25c17b9423335fb150
you7588 <yo7588@gmail.com>
2017-07-12 09:37:30 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join ,:quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
@@ -38,6 +38,32 @@ class GroupsController < ApplicationController
   def destroy
     @group.destroy
     redirect_to groups_path, alert: "delete success"
+  end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "Join in sucessful!"
+    else
+      flash[:warning] = "You alreadly is member!"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "You already quit the group!"
+    else
+      flash[:warning] = "You are not a group member,how to quit, XDDD"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root 'groups#index'
 
   resources :groups do
+    member do
+      post :join
+      post :quit
+    end
     resources :posts
   end
 end
